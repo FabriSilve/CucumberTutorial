838454264f2b5fce39e145796cf8d5f7843c4c9d
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-11-27 07:56:33 +0100

    Remove warning avoiding deprecated method

--- steps/Utils.steps.js
+++ steps/Utils.steps.js
@@ -1,26 +1,36 @@
-const defineSupportCode = require('cucumber').defineSupportCode;
+const Before = require('cucumber').Before;
+const Given = require('cucumber').Given;
+const Then = require('cucumber').Then ;
+const When = require('cucumber').When;
+
 const assert = require('assert');
+
 const Utils = require('../src/Utils');
 
-defineSupportCode(function({ Before, Given, Then, When }) {
-  let util;
 
-  Before(function () {
-    util = null;
-  });
+let util;
 
-  Given('I start with {int}', function (input) {
-    util = new Utils(input);
-  });
+Before(function () {
+  util = null;
+});
 
-  When('I add {int}', function (input) {
-    util.add(input);
-  });
-  When('I multiply by {int}', function (input) {
-    util.times(input);
-  });
+Given('I start with {int}', function (input) {
+  util = new Utils(input);
+});
 
-  Then('I end up with {int}', function (input) {
-    assert.equal(util.result(), input);
-  });
+When('I add {int}', function (input) {
+  util.add(input);
+});
+When('I multiply by {int}', function (input) {
+  util.times(input);
+});
+When('I add the following numbers:', function (table) {
+  table.raw()
+    .map(row => row[0])
+    .map(v => util.add(v))
+});
+
+
+Then('I end up with {int}', function (input) {
+  assert.equal(util.result(), input);
 });
